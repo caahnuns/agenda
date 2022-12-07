ceab5be72a634d1edefd8d6ef4517e88dab28f1e
Camila Melo <camila.crnm@gmail.com>
2022-12-07 20:27:35 -0300

    Commit inicial com estrutura do projeto

---
+++ src/model/Contato.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contato {
+
+    private String nome;
+    private String sobrenome;
+    private List<Telefone> telefones;
+    private List<Endereco> enderecos;
+
+    public Contato(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+
+        this.telefones = new ArrayList<>();
+        this.enderecos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+}
